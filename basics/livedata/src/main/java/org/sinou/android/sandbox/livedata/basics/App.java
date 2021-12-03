44bd5da9b20590ede8b639fe205d698bbb12be78
Bruno Sinou <bruno.sinou@posteo.de>
2021-12-03 16:33:06 +0100

    Reorganise sandbox

---
+++ basics/livedata/src/main/java/org/sinou/android/sandbox/livedata/basics/App.java
@@ -0,0 +1,11 @@
+package org.sinou.android.sandbox.livedata.basics;
+
+import android.app.Application;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
