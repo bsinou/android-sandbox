1eb67196040856a18ad2644a622e39b0eaaed158
Bruno Sinou <bruno.sinou@posteo.de>
2021-12-07 14:32:40 +0100

    Show case basic activity template for reference

---
+++ basics/appbar/src/main/java/org/sinou/android/sandbox/appbar/basics/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package org.sinou.android.sandbox.appbar.basics.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
